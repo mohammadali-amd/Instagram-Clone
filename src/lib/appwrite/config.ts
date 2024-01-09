fd7d4192e6d5a226a84c6cdf3243b717add43fc2
mohammadali-amd <amidi1380@gmail.com>
2024-01-10 01:13:04 +0330

    Initial Commit

---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,16 @@
+import { Account, Client, Avatars, Storage, Databases } from 'appwrite'
+
+export const appwriteConfig = {
+   projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+   url: import.meta.env.VITE_APPWRITE_URL,
+}
+
+export const client = new Client();
+
+client.setProject(appwriteConfig.projectId);
+client.setEndpoint(appwriteConfig.url);
+
+export const account = new Account(client);
+export const avatars = new Avatars(client);
+export const storage = new Storage(client);
+export const databases = new Databases(client);
