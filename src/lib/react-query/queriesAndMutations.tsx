8cbce844a4256c9891e8713cb810f34d20483026
mohammadali-amd <amidi1380@gmail.com>
2024-03-17 12:58:52 +0330

    Explore Page

--- src/lib/react-query/queriesAndMutations.tsx
+++ src/lib/react-query/queriesAndMutations.tsx
@@ -1,6 +1,6 @@
 import { useQuery, useMutation, useQueryClient, useInfiniteQuery, } from '@tanstack/react-query';
 import { INewPost, INewUser, IUpdatePost } from '@/types';
-import { createPost, createUserAccount, deletePost, deleteSavedPost, getCurrentUser, getPostById, getRecentPosts, likePost, savePost, signInAccount, signOutAccount, updatePost } from '../appwrite/api';
+import { createPost, createUserAccount, deletePost, deleteSavedPost, getCurrentUser, getInfinitePosts, getPostById, getRecentPosts, likePost, savePost, searchPosts, signInAccount, signOutAccount, updatePost } from '../appwrite/api';
 import { QUERY_KEYS } from './queryKeys';
 
 export const useCreateUserAccount = () => {
@@ -141,5 +141,27 @@ export const useDeletePost = () => {
             queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
          })
       }
+   })
+}
+
+export const useGetPosts = () => {
+   return useInfiniteQuery({
+      queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+      queryFn: getInfinitePosts,
+      getNextPageParam: (lastPage) => {
+         if (lastPage && lastPage.documents.length === 0) return null;
+
+         const lastId = lastPage.documents[lastPage?.documents.length - 1].$id;
+
+         return lastId;
+      }
+   })
+}
+
+export const useSearchPosts = (searchTerm: string) => {
+   return useQuery({
+      queryKey: [QUERY_KEYS.SEARCH_POSTS],
+      queryFn: () => searchPosts(searchTerm),
+      enabled: !!searchTerm
    })
 }
